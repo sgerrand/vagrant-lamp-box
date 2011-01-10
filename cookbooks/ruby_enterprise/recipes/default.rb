5509b8a0935cf73a76e82cb75359ee5fc9d56111
Sasha Gerrand <sasha@returnity.com>
2011-01-10 12:52:27 +1100

    - Added ruby and ruby enterprise cookbooks - Updated main vagrant cookbook to use ruby in base install

---
+++ cookbooks/ruby_enterprise/recipes/default.rb
@@ -0,0 +1,101 @@
+#
+# Cookbook Name:: ruby_enterprise
+# Recipe:: default
+#
+# Author:: Joshua Timberman (<joshua@opscode.com>)
+# Author:: Sean Cribbs (<seancribbs@gmail.com>)
+# Author:: Michael Hale (<mikehale@gmail.com>)
+# 
+# Copyright:: 2009-2010, Opscode, Inc.
+# Copyright:: 2009, Sean Cribbs
+# Copyright:: 2009, Michael Hale
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if ( node[:platform] == 'ubuntu' )
+  if ( node[:kernel][:machine] == 'x86_64' )
+    kernel_machine = 'amd64'
+  else
+    kernel_machine = 'i386'
+  end
+
+  remote_file "/tmp/ruby-enterprise_#{node[:ruby_enterprise][:version]}_#{kernel_machine}_#{node[:platform]}#{node[:platform_version]}.deb" do
+    source "http://rubyforge.org/frs/download.php/71099/ruby-enterprise_#{node[:ruby_enterprise][:version]}_#{kernel_machine}_#{node[:platform]}#{node[:platform_version]}.deb"
+  end
+   
+  bash "Install Ruby Enterprise Edition from deb" do
+    cwd "/tmp"
+    code <<-EOH
+    dpkg -i ruby-enterprise_#{node[:ruby_enterprise][:version]}_#{kernel_machine}_#{node[:platform]}#{node[:platform_version]}.deb
+    EOH
+  end
+
+  %w{apache2-prefork-dev libapr1-dev libaprutil1-dev}.each do |pkg|
+    package pkg
+  end
+
+#  passenger_version = system("/usr/local/bin/passenger-config --version")
+#  passenger_version = `/usr/local/bin/passenger-config --version`
+#  puts "Passenger version: "+passenger_version
+#  exit
+
+#  execute "passenger-config" do
+#    command "/usr/local/bin/passenger-config --version"
+#    action :run
+#  end
+
+  passenger_version = '2.2.14'
+
+  bash "Install custom Phusion Passenger module" do
+    code <<-EOH
+    /usr/local/bin/passenger-install-apache2-module --auto
+    echo "LoadModule passenger_module /usr/local/lib/ruby/gems/1.8/gems/passenger-#{passenger_version}/ext/apache2/mod_passenger.so" > /etc/apache2/mods-available/passenger.load
+    echo "PassengerRoot /usr/local/lib/ruby/gems/1.8/gems/passenger-#{passenger_version}" > /etc/apache2/mods-available/passenger.conf
+    echo "PassengerRuby /usr/local/bin/ruby" >> /etc/apache2/mods-available/passenger.conf
+    a2enmod passenger
+    EOH
+
+#    if do
+#      ::File.exists?("/usr/local/bin/passenger-config")
+#    end
+  end
+
+else
+
+  include_recipe "build-essential"
+
+  %w{ libssl-dev libreadline5-dev }.each do |pkg|
+    package pkg
+  end
+
+  remote_file "/tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz" do
+    source "#{node[:ruby_enterprise][:url]}.tar.gz"
+    not_if { ::File.exists?("/tmp/ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz") }
+  end
+
+  bash "Install Ruby Enterprise Edition" do
+    cwd "/tmp"
+    code <<-EOH
+    tar zxf ruby-enterprise-#{node[:ruby_enterprise][:version]}.tar.gz
+    ruby-enterprise-#{node[:ruby_enterprise][:version]}/installer \
+      --auto=#{node[:ruby_enterprise][:install_path]} \
+      --dont-install-useful-gems
+    EOH
+    not_if do
+      ::File.exists?("#{node[:ruby_enterprise][:install_path]}/bin/ree-version") &&
+      system("#{node[:ruby_enterprise][:install_path]}/bin/ree-version | grep -q '#{node[:ruby_enterprise][:version]}$'")
+    end
+  end
+
+end
--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -1,9 +1,27 @@
 require_recipe "apt"
 require_recipe "apache2"
-#require_recipe "mysql::server"
+require_recipe "mysql::libs"
+require_recipe "mysql::server"
+
+# PHP 5 -- normal 8.04
 require_recipe "php::php5"
-require_recipe "php::module_sqlite3"
-require_recipe "sqlite"
+require_recipe "php::module_curl"
+require_recipe "php::module_gd"
+require_recipe "php::module_mcrypt"
+require_recipe "php::module_mysql"
+#require_recipe "php::module_memcache"
+##require_recipe "php::module_sqlite3"
+##require_recipe "sqlite"
+
+# Ruby on Rails -- Returnity 8.04
+require_recipe "ruby_enterprise"
+
+# Ruby on Rails -- normal 10.04
+#require_recipe "ruby"
+#require_recipe "ruby::gems"
+#require_recipe "ruby::rails"
+#require_recipe "ruby::module_haml"
+#require_recipe "ruby::module_mysql"
 
 execute "disable-default-site" do
   command "sudo a2dissite default"
